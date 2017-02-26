cc3f2e86b9bde3611ffb23ec65cada9767f9025a
Jesus Chavez <jachavez21@miners.edu>
2017-02-26 12:12:37 -0700

    almost

--- stopWait/client/udpClient.py
+++ stopWait/client/udpClient.py
@@ -1,50 +1,27 @@
 #! /bin/python
-import time
 import sys, re  
 from socket import *
-from threading import Thread 
+import json
 # default params
 serverAddr = ('localhost', 50001)       
 
 #file location
 fileLoc = '~/'
-                       
-def run(conditions):
-    state = conditions['request']
-    filename = conditions['filename']
-    serverAddr = conditions['serverAddr']
-    data = ""
 
-    while state != "close":
-        clientSocket = socket(AF_INET, SOCK_DGRAM)
-        clientSocket.settimeout(5)
-        if state == "get": 
-            try:
-                clientSocket.sendto("get:%s" % filename, serverAddr)
-                response, serverAddrPort = clientSocket.recvfrom(2048)
-                if response == "ack_filename":
-                    state = "wait_data"
-            except timeout:
-                print("timedout, trying again")
-        if state == "wait_data":
-            clientSocket.sendto("", serverAddr)
-            response, serverAddrPort = clientSocket.recvfrom(2048)
-            #if response != "":
-            state = "ack_data"
-            data = response
-        if state == "ack_data":
-            clientSocket.sendto("", serverAddr)
-            response, serverAddrPort = clientSocket.recvfrom(2048)
-            #if response == "close":
-            state = "close"
-        print(timeout);
-        #time.sleep(timeout)
-        clientSocket.close()
+print("sending")
+s_1 = {'request':'get', 'params':'file1'}
+s_2 = json.dumps(s_1)
+n = 0
+state = 0
+while 1:
+    clientSocket = socket(AF_INET, SOCK_DGRAM)
+    clientSocket.settimeout(5)
+    clientSocket.sendto(s_2, serverAddr)
+    response, serverAddrPort = clientSocket.recvfrom(2048)
+    if response:
+        s_1 = {'request':'ack', 'params':n}
+        s_2 = json.dumps(s_1)
+        n += 1
         
-conditions = {'request':'get', 
-    'filename':'testfile.txt', 
-    'serverAddr': ('localhost', 50001)
-    }
-
-t = Thread(target=run, args=(conditions,))
-t.start()
+    clientSocket.close()
+    print(response)
--- stopWait/server/udpServer.py
+++ stopWait/server/udpServer.py
@@ -1,19 +1,68 @@
 #! /bin/python
 from socket import *
 import sys
+import json
 
-state = "init"
+def fileExists(params):
+    return params == fileName
+    
+_S_INIT = 's_init'
+_S_SEND = 's_send'
+_S_WAIT = 's_wait'
+
+state = _S_INIT
+ackCount = 0;
+
+fileName = "file1"
+fileCont = {0:'0_o', 1:'1_o', 2:'2_o'}
+
 # default params
 serverAddr = ("", 50001)
 
 print "binding datagram socket to %s" % repr(serverAddr)
+
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(serverAddr)
+
 print "ready to receive"
+
 while 1:
-    request, clientAddrPort = serverSocket.recvfrom(2048)
+    message, clientAddrPort = serverSocket.recvfrom(2048)
     print "from %s: rec'd '%s'" % (repr(clientAddrPort), message)
+    data = json.loads(message)
+    request = data['request']
+    params = data['params']
     
-    if request == "get:%s":
-        modifiedMessage = message.upper()
-        serverSocket.sendto("ack_filename", clientAddrPort)
+    if state == _S_INIT:
+        if request == "get" and fileExists(params):
+            serverSocket.sendto(fileCont[ackCount], clientAddrPort)
+            state = _S_WAIT
+        elif request == "get" and not fileExists(params):
+            serverSocket.sendto("404", clientAddrPort)
+            state = _S_INIT
+        
+    elif state == _S_WAIT:  
+        if request == "ack":
+            if ackCount not in fileCont:
+                serverSocket.sendto("close", clientAddrPort)
+                state = _S_INIT
+                ackCount = 0
+            elif params == ackCount:
+                ackCount += 1
+                serverSocket.sendto(fileCont[ackCount], clientAddrPort)
+                state = _S_WAIT
+            elif params < ackCount and params >= 0:
+                serverSocket.sendto(fileCont[params], clientAddrPort)
+                state = _S_WAIT
+                
+        elif request == "get":
+            # Case where the server answer for data[0] was lost after client started get
+            if fileExists(params):
+                serverSocket.sendto(fileCont[0], clientAddrPort)
+                state = _S_WAIT
+            elif not fileExists(params):
+                serverSocket.sendto("404", clientAddrPort)
+                state = _S_INIT
+                
+    print(state);
+    
