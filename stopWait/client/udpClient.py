cadb56174227358d1854a9f06cf7d1498915a2ba
Jesus Chavez <jachavez21@miners.edu>
2017-02-26 19:28:05 -0700

    working

--- stopWait/client/udpClient.py
+++ stopWait/client/udpClient.py
@@ -2,29 +2,57 @@
 import sys, re  
 from socket import *
 import json
-
+from array import array
 #prototypes and functions
+
 def buildRequest(requset, param):
     return json.dumps({'request':requset, 'params':param})
+def usage():
+    print "usage: %s --request [get 'or' put] --serverAddr [host:port] -f [filename]"  % sys.argv[0]
+    sys.exit(1)
 
+#constants
 _C_INIT = "c_initial"       # state definition: initial
 _C_WAIT = "c_waiting"       # state definition: waiting for data
-_C_ACK = "c_acking"  # state definition: accepting data
-# default params
-serverAddr = ('localhost', 50000)
+_C_ACK = "c_acking"         # state definition: accepting data
 
+#object fields
+serverAddr = ('', 50001)
+initRequest = buildRequest("get", "file1.txt")
 state = _C_INIT             # current state
 ackCount = 0                # count of successful transmit
-timeout = .025                 # timeout for socket thread
-fileName = 'file1'          # file location
+timeout = .025              # timeout for socket thread 
 packegeDropCount = 0
-
+buffer = []
+bufferSize = 0.0
+#requests and usage
+try:
+    args = sys.argv[1:]
+    while args:
+        sw = args[0]; del args[0]
+        if sw == "--request":
+            request = re.split("", args[0]); del args[0]
+            if request != "put" or request != "get":
+                print "unexpected request ->  %s" % args[0]
+                usage()
+            initRequest = buildRequest(request, fileName)
+        if sw == "--serverAddr":
+            addr, port = re.split(":", args[0]); del args[0]
+            serverAddr = (addr, int(port))
+        if sw == "-f":
+            fileName = re.split("", args[0]); del args[0]
+        else:
+            print "unexpected parameter %s" % args[0] 
+            usage()         
+except:
+    usage()
+    
 while 1:
     print("sending")
     print ("state: %s" % state)
     print ("ack[%s]" % ackCount)
     if state == _C_INIT:
-        message = buildRequest("get", fileName)
+        message = initRequest     
     elif state == _C_WAIT or state == _C_ACK:
         message = buildRequest("ack", ackCount)
         
@@ -47,15 +75,28 @@ while 1:
             break;
         elif response == "404":
             print ("404, unknown file")
-            state = _C_INIT
-        elif state == _C_WAIT or state == _C_INIT:
+            packegeDropCount = .005
+            timeout =  .025
+            usage()
+            break;
+        elif state == _C_INIT:
+            responseArguments= response.split(':')
+            bufferSize = responseArguments[0]
+            buffer = [None]*int(bufferSize)
+            buffer[0] = responseArguments[1]
+            state = _C_ACK
+        elif state == _C_WAIT:
             state = _C_ACK
         elif state == _C_ACK:
+            buffer[ackCount+1] = response
             ackCount += 1
             state = _C_ACK
+            
         print(response)
         clientSocket.close()
-    
+print("full msg")
+
+print(filter(None, buffer))
         
        
        
--- stopWait/server/udpServer.py
+++ stopWait/server/udpServer.py
@@ -2,9 +2,6 @@
 from socket import *
 import sys
 import json
-
-def fileExists(params):
-    return params == fileName
     
 _S_INIT = 's_init'
 _S_SEND = 's_send'
@@ -13,9 +10,40 @@ _S_WAIT = 's_wait'
 state = _S_INIT
 ackCount = 0;
 
-fileName = "file1"
-fileCont = {0:'0_o', 1:'1_o', 2:'2_o'}
+fileBuffer = []
+fileSize = 0
+step = 0
+fileSize = 0
 
+def fileExists(params):
+    try:
+       file_dl = open(params, 'r') #for size
+       fileSize = len(file_dl.read())
+       file_dl.close()
+       file_dl = open(params, 'r') #for lines
+       window = 10
+       if fileSize > 1000:
+           window = 100
+       elif fileSize >100:
+           window = 10
+       else:
+           window = 2
+       step = fileSize / window
+       fileBuffer = [None]*window
+       index = 0
+       segment = '$'
+       print(fileSize)
+       while 1:
+           segment =file_dl.read(step)
+           if segment == '':
+               break
+           fileBuffer[index] = file_dl.read(step)
+           index+=1
+       return (window, fileBuffer, step, 1)
+    except:
+        raise
+        return 0
+    
 # default params
 serverAddr = ("", 50001)
 
@@ -32,12 +60,13 @@ while 1:
     data = json.loads(message)
     request = data['request']
     params = data['params']
-    
     if state == _S_INIT:
-        if request == "get" and fileExists(params):
-            serverSocket.sendto(fileCont[ackCount], clientAddrPort)
+        fileSize, fileBuffer, step, condition = fileExists(params)
+        print(fileBuffer)
+        if request == "get" and condition!= 0:
+            serverSocket.sendto("%d:%s"%(fileSize, fileBuffer[ackCount]), clientAddrPort)
             state = _S_WAIT
-        elif request == "get" and not fileExists(params):
+        elif request == "get" and condition !=0:
             serverSocket.sendto("404", clientAddrPort)
             state = _S_INIT
         else: 
@@ -48,21 +77,21 @@ while 1:
         if request == "ack":
             if params == ackCount:
                 ackCount += 1
-                if ackCount not in fileCont:
+                if ackCount >= len(fileBuffer) or fileBuffer[ackCount] == None:
                     serverSocket.sendto("close", clientAddrPort)
                     state = _S_INIT
                     ackCount = 0
-                else:
-                    serverSocket.sendto(fileCont[ackCount], clientAddrPort)
-                    state = _S_WAIT     
-            elif params < ackCount and params >= 0:
-                serverSocket.sendto(fileCont[params], clientAddrPort)
+                elif fileBuffer[ackCount] != None:
+                    serverSocket.sendto(fileBuffer[ackCount], clientAddrPort)
+                    state = _S_WAIT 
+            elif params < ackCount and params >= 0 and fileBuffer[ackCount] != None:
+                serverSocket.sendto(fileBuffer[params], clientAddrPort)
                 state = _S_WAIT
                 
         elif request == "get":
             # Case where the server answer for data[0] was lost after client started get
             if fileExists(params):
-                serverSocket.sendto(fileCont[0], clientAddrPort)
+                serverSocket.sendto(fileBuffer[0], clientAddrPort)
                 state = _S_WAIT
             elif not fileExists(params):
                 serverSocket.sendto("404", clientAddrPort)
