b1764053cf5a9223f1aba935278b8d71576e2c63
Jesus Chavez <jachavez21@miners.edu>
2017-03-10 22:00:56 -0700

    working on parser for json

--- jsonParse.py
+++ jsonParse.py
@@ -6,7 +6,14 @@
 # @return python representation of json
 import sys, re  
 def jsonParseThis(request, params):
-    return "{request:%s, params:%s}" % (request, params)
-def jsonDeParseThis(request, params):
-    return "{request:%s, params:%s}" % (request, params)
-
+    return "{'request':'%s', 'params':'%s'}" % (request, params)
+def jsonDeParseThis(input):
+    #print input
+    #m = re.match(r"(?P<request>\w+) (?P<params>\w+)", input)
+    #m.groupdict()
+    m = re.match(r"{'request':'(?P<request>\w+)', 'params':'(?P<params>\w+)'}", input)
+    print m.groupdict()
+    
+m_test = jsonParseThis("get","file2")
+print m_test
+jsonDeParseThis(m_test)
