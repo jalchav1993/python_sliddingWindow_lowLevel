5d359393c0addc3025805aca2e690137d8e1cad6
Jesus Chavez <jachavez21@miners.edu>
2017-03-10 22:45:44 -0700

    finished testing of manual json parse since API is not allowed

--- jsonParse.py
+++ jsonParse.py
@@ -6,14 +6,14 @@
 # @return python representation of json
 import sys, re  
 def jsonParseThis(request, params):
-    return "{'request':'%s', 'params':'%s'}" % (request, params)
+    return "{\"params\": \"%s\", \"request\": \"%s\"}" % (params, request)
 # ref:https://docs.python.org/2/library/re.html
 # @name Json Parser
 # @author Jesus Chavez
 # @funct jsonParseThis() manual parser for json inputs 
 # @return python representation of json
 def jsonDeParseThis(input):
-    regExpression = re.compile(r"{'request':'(?P<request>\w+)', 'params':'(?P<params>\w+)'}")
+    regExpression = re.compile(r"{\"params\": \"(?P<params>\w+)\", \"request\": \"(?P<request>\w+)\"}")
     m = re.match(regExpression, input)
     return m.groupdict()
 
--- jsonParseUnitTest.py
+++ jsonParseUnitTest.py
@@ -1,7 +1,36 @@
 #! /bin/python
 #! /Users/aexchv/Desktop/Nets/udp-file-transfer-coreunlimied/jsonParse.py
-from jsonParse import *
+# ref:https://docs.python.org/2/library/re.html
+# @name Json Parser Unit Tester
+# @author Jesus Chavez
 
-def test():
-    print jsonParseThis("put", "file")
-test()
+from jsonParse import *
+import json
+
+#json api
+def buildRequest(request, param):
+    return json.dumps({'request':request, 'params':param})
+    
+#json api
+def extractRequest(input):
+    return json.loads(input)
+
+#testing json loads
+def testParsing():
+    expected = buildRequest("put", "file")
+    actual = jsonParseThis("put", "file")
+    print "expected %s \n"%expected
+    print "actual %s \n"%actual
+    return actual == expected
+    
+def testDeparsing():
+    in_expected = buildRequest("put", "file")
+    in_actual = jsonParseThis("put", "file")
+    expected = extractRequest(in_expected)
+    actual = jsonDeParseThis(in_actual)
+    print "expected %s \n"%expected
+    print "actual %s \n"%actual
+    return actual == expected
+    
+print "testing parsing %s" % testParsing()
+print "testing deparsing %s" % testDeparsing()
