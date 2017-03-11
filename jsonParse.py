0e864653870022a52c7f72eb5944d57e25c1f004
Jesus Chavez <jachavez21@miners.edu>
2017-03-10 22:02:40 -0700

    doc

--- jsonParse.py
+++ jsonParse.py
@@ -7,13 +7,15 @@
 import sys, re  
 def jsonParseThis(request, params):
     return "{'request':'%s', 'params':'%s'}" % (request, params)
+# ref:https://docs.python.org/2/library/re.html
+# @name Json Parser
+# @author Jesus Chavez
+# @funct jsonParseThis() manual parser for json inputs 
+# @return python representation of json
 def jsonDeParseThis(input):
     #print input
     #m = re.match(r"(?P<request>\w+) (?P<params>\w+)", input)
     #m.groupdict()
     m = re.match(r"{'request':'(?P<request>\w+)', 'params':'(?P<params>\w+)'}", input)
-    print m.groupdict()
-    
-m_test = jsonParseThis("get","file2")
-print m_test
-jsonDeParseThis(m_test)
+    return m.groupdict()
+
